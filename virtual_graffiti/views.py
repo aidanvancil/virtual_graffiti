c47f37ef59a15438d620648b87d66c8b75abed24
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2024-01-28 17:20:18 -0800

    chore: opencv script migrate to views.py
    Work done by Moises + Foster.

--- virtual_graffiti/views.py
+++ virtual_graffiti/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login as log, logout as auth_logou
 from django.http import StreamingHttpResponse, JsonResponse
 from django.views.decorators import gzip
 from app.models import UserProfile, Laser
+from screeninfo import get_monitors
+import random
 import cv2
 import qrcode
 import base64
@@ -42,6 +44,169 @@ def get_laser(request, laser_id):
         
     return JsonResponse({'success': False, 'error': 'Invalid request method'}, status=405)
 
+def enumerate_cameras():
+    index = 0
+    arr = []
+    while True:
+        cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
+        if not cap.read()[0]:
+            break
+        arr.append(index)
+        cap.release()
+        index += 1
+    return arr
+
+def is_laser_contour(contour, hsv_frame, min_area=20, max_area=200):
+    # Check the contour area
+    if not min_area < cv2.contourArea(contour) < max_area:
+        return False
+
+    # Further checks can be added here (e.g., brightness, shape)
+    # ...
+
+    return True
+
+def color_segmentation(frame, lower_color, upper_color):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    segmented = cv2.bitwise_and(frame, frame, mask=mask)
+    segmented_gray = cv2.cvtColor(segmented, cv2.COLOR_BGR2GRAY)
+    return segmented_gray
+
+def load_scaled_image(image_path, width, height):
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    if image is None:
+        raise ValueError("Image not found")
+    return cv2.resize(image, (width, height))
+
+def update_canvas_with_image(canvas, background_image, x, y, scale_factor, radius=5):
+    scaled_x = int(x * scale_factor)
+    scaled_y = int(y * scale_factor)
+    mask = np.zeros(canvas.shape[:2], dtype=np.uint8)
+    cv2.circle(mask, (scaled_x, scaled_y), radius, 255, -1)
+    mask_indices = np.where(mask > 0)
+    canvas[mask_indices] = background_image[mask_indices]
+
+def count_filled_pixels(canvas, background_image):
+    # Count pixels that are not black (0, 0, 0) in the canvas
+    filled_pixels = np.sum(np.any(canvas != [0, 0, 0], axis=2))
+    total_pixels = background_image.shape[0] * background_image.shape[1]
+    return filled_pixels, total_pixels
+
+def apply_glitter_effect(canvas, background_image, iterations=400, intensity=600, delay=8):
+    for _ in range(iterations):
+        for _ in range(intensity):
+            x, y = random.randint(0, canvas.shape[1] - 1), random.randint(0, canvas.shape[0] - 1)
+            if np.all(canvas[y, x] == [0, 0, 0]):  # Check if the pixel is unfilled
+                canvas[y, x] = background_image[y, x]
+        cv2.imshow(canvas_window_name, canvas)
+        cv2.waitKey(delay)
+
+def initialize_projector(request):
+    red_lower = np.array([0, 100, 100])
+    red_upper = np.array([10, 255, 255])
+    green_lower = np.array([40, 100, 100])
+    green_upper = np.array([80, 255, 255])
+    purple_upper = np.array([130, 50, 50])
+    purple_lower = np.array([160, 255, 255])
+
+    mode = print("Choose mode (fill/free): ")
+    if mode not in ['fill', 'free']:
+        print("Invalid mode selected. Exiting.")
+    
+    camera_indexes = enumerate_cameras()
+
+    if len(camera_indexes) == 0:
+        print("No cameras found.")
+        exit()
+
+    cap = cv2.VideoCapture(camera_indexes[0], cv2.CAP_DSHOW)
+
+    screen_width = cap.get(3) 
+    screen_height = cap.get(4)
+    print(screen_height)
+    print(screen_width) 
+    
+    canvas_width, canvas_height = int(screen_width), int(screen_height)
+    
+    scale_factor_x = canvas_width / screen_width
+    scale_factor_y = canvas_height / screen_height
+    scale_factor = min(scale_factor_x, scale_factor_y)
+
+    background_image = None
+    if mode == 'fill':
+        background_image = load_scaled_image(r"C:\Users\aidan\Pictures\Screenshots\water.png", canvas_width, canvas_height)
+        background_image = background_image[:, :, :3]
+
+    canvas = np.zeros((canvas_height, canvas_width, 3), dtype=np.uint8)
+
+    canvas_window_name = 'Canvas'
+    
+    cv2.namedWindow(canvas_window_name, cv2.WINDOW_NORMAL)
+    
+    
+    # Detect if an external monitor is present and adjust window position
+    monitors = get_monitors()
+    if len(monitors) > 1:
+        # Assume you want to display on the second monitor (external monitor)
+        external_monitor = monitors[1]
+        cv2.moveWindow(canvas_window_name, external_monitor.x, external_monitor.y)
+
+    
+    cv2.setWindowProperty(canvas_window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    FILL_THRESHOLD_PERCENT = 0.80
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        red_segmented = color_segmentation(frame, red_lower, red_upper)
+        green_segmented = color_segmentation(frame, green_lower, green_upper)
+        purple_segmented = color_segmentation(frame, purple_lower, purple_upper)
+
+        # Process for both rgb lasers in both modes
+        for color_index, segmented in enumerate([red_segmented, green_segmented, purple_segmented]):
+            contours, _ = cv2.findContours(segmented, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            for contour in contours:
+                if is_laser_contour(contour, hsv_frame):
+                    moments = cv2.moments(contour)
+                    if moments["m00"] != 0:
+                        cx = int(moments["m10"] / moments["m00"])
+                        cy = int(moments["m01"] / moments["m00"])
+
+                        if mode == 'fill' and background_image is not None:
+                            update_canvas_with_image(canvas, background_image, cx, cy, scale_factor)
+
+                            filled_pixels, total_pixels = count_filled_pixels(canvas, background_image)
+                            fill_percentage = filled_pixels / total_pixels
+
+                            if fill_percentage >= FILL_THRESHOLD_PERCENT:
+                                # Apply glitter effect before filling the entire image
+                                apply_glitter_effect(canvas, background_image)
+                                # Fill in the entire image
+                                canvas[:, :] = background_image[:, :]
+                        elif mode == 'free':
+                            color = (0, 0, 255) if color_index == 0 else (0, 255, 0)
+                            cv2.circle(canvas, (cx, cy), 2, color, -1)
+
+        cv2.imshow('Original', frame)
+        cv2.imshow(canvas_window_name, canvas)
+        # Display the original frame and canvas
+                
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
 @gzip.gzip_page
 def video_feed(request):
     cap = cv2.VideoCapture(1)
@@ -49,52 +214,13 @@ def video_feed(request):
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
 
-    color_ranges = {
-        'red': ([0, 100, 100], [5, 255, 255]),
-        'purple': ([130, 50, 50], [160, 255, 255]),
-        'green': ([50, 50, 50], [80, 255, 255]),
-    }
-
-    def get_color(color):
-        if color == 'red':
-            return (0, 0, 255)
-        elif color == 'purple':
-            return (255, 0, 255)
-        elif color == 'green':
-            return (0, 255, 0)
-        else:
-            return (255, 255, 255)
-        
     def generate():
         try:
             while True:
                 ret, frame = cap.read()
                 if not ret:
                     break
-
-                hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-                color_centers = {'red': [], 'purple': [], 'green': []}
-
-                for color, (lower, upper) in color_ranges.items():
-                    mask = cv2.inRange(hsv, np.array(lower), np.array(upper))
-
-                    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                    largest_contour = max(contours, key=cv2.contourArea, default=None)
-
-                    if largest_contour is not None:
-                        M = cv2.moments(largest_contour)
-                        if M["m00"] != 0:
-                            cx = int(M["m10"] / M["m00"])
-                            cy = int(M["m01"] / M["m00"])
-                            color_centers[color] = (cx, cy)
-
-                for color, center in color_centers.items():
-                    if center:
-                        cv2.circle(frame, center, 10, get_color(color), -1)
-
-                        
-
+            
                 _, jpeg = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
                 frame_bytes = jpeg.tobytes()
 
