994ca232827205dc207709ef68ad9634716e2f61
1001-Aidan-Vancil <42700427+aidanvancil@users.noreply.github.com>
2024-05-03 00:11:40 -0700

    feat: online mode

--- virtual_graffiti/urls.py
+++ virtual_graffiti/urls.py
@@ -45,12 +45,9 @@ urlpatterns = [
     path('disconnect/<str:first_name>_<str:last_name>/', views.remove_user_and_release_laser, name='disconnect_user'),
     path('store_code/', views.store_code, name='store_code'),
     path('del_code/', views.del_code, name='del_code'),
+    path('get_and_set_lasers/<int:code>', views.get_and_set_lasers, name='get_and_set_lasers'),
     path('pull/<str:mode>', admin_views.pull, name='pull'),
     path('submit_image/', admin_views.submit_image, name='submit_image'),
     path('_init/', admin_views.init, name='_init'),
-    path('set_laser_color/<str:laser_id>/', views.set_laser_color, name='set_laser_color'),
-    path('set_laser_size/<str:laser_id>/', views.set_laser_size, name='set_laser_size'),
-    path('set_laser_style/<str:laser_id>/', views.set_laser_style, name='set_laser_style'),
-    path('get_laser/<str:laser_id>/', views.get_laser, name='get_laser'),
     re_path(r'^.*/$', views.errors, name='errors'),
 ]
